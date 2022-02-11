dd707eb9b625076d82869887f1ef7ab7baf06dff
Matheus da silva <ms25022003@gmail.com>
2022-02-11 14:48:07 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const btnMobile = document.getElementById("btn-mobile")
+btnMobile.addEventListener("click", () => {
+const nav = document.getElementById("menu")
+    nav.classList.toggle("show")
+})
